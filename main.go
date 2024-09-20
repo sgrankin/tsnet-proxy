7b5ae843e0225b5f655488c44defbbe9da5137b8
Sergey Grankin <sgrankin@gmail.com>
2024-09-19 21:26:38 -0400

    Improve HTTPS support
    - Fix HSTS
    - Use permanent 308 redirect

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ import (
 
 	"tailscale.com/client/tailscale"
 	"tailscale.com/tsnet"
-	"tailscale.com/util/dnsname"
 )
 
 func main() {
@@ -75,6 +74,12 @@ func run(srv *tsnet.Server, httpAddr string, useHTTPS bool, proxyConf []proxyCon
 		return fmt.Errorf("tailscale status: %v", err)
 	}
 
+	enableTLS := useHTTPS && status.Self.HasCap(tailcfg.CapabilityHTTPS) && len(srv.CertDomains()) > 0
+	fqdn := strings.TrimSuffix(status.Self.DNSName, ".")
+	if useHTTPS && !enableTLS {
+		return fmt.Errorf("HTTPS requested but unavailable; check your tailscale config")
+	}
+
 	var handler http.Handler = &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			r.URL.Scheme = "http"
@@ -85,14 +90,15 @@ func run(srv *tsnet.Server, httpAddr string, useHTTPS bool, proxyConf []proxyCon
 				return net.Dial("tcp", httpAddr)
 			},
 		},
+		ModifyResponse: func(response *http.Response) error {
+			if enableTLS {
+				// Set STS to have the browser always use https.
+				response.Header.Set("Strict-Transport-Security", "max-age=31536000")
+			}
+			return nil
+		},
 	}
 	handler = setAuthHeaders(localClient, handler)
-
-	enableTLS := useHTTPS && status.Self.HasCap(tailcfg.CapabilityHTTPS) && len(srv.CertDomains()) > 0
-	fqdn := strings.TrimSuffix(status.Self.DNSName, ".")
-	if useHTTPS && !enableTLS {
-		return fmt.Errorf("HTTPS requested but unavailable; check your tailscale config")
-	}
 
 	g, ctx := errgroup.WithContext(ctx)
 	if enableTLS {
@@ -176,16 +182,15 @@ func proxy(srv *tsnet.Server, network, listenAdr, dialAddr string) error {
 	defer lis.Close()
 	for {
 		conn1, err := lis.Accept()
-		if err != nil {
-			if errors.Is(err, net.ErrClosed) {
-				err = nil
-			}
+		if errors.Is(err, net.ErrClosed) {
+			return nil
+		} else if err != nil {
 			return err
 		}
-		defer conn1.Close()
 		log.Printf("Accepted connection on %s", listenAdr)
 		conn2, err := net.Dial(network, dialAddr)
 		if err != nil {
+			conn1.Close()
 			return err
 		}
 		go func() {
@@ -218,56 +223,30 @@ func pipe(conn1, conn2 net.Conn) error {
 }
 
 func listenHTTPS(srv *tsnet.Server, handler http.Handler, addr string) error {
-	httpsHandler := HSTS(handler)
-	httpsListener, err := srv.ListenTLS("tcp", addr)
+	lis, err := srv.ListenTLS("tcp", addr)
 	if err != nil {
 		return err
 	}
-	defer httpsListener.Close()
-	if err := http.Serve(httpsListener, httpsHandler); err != nil {
-		return err
-	}
-	return nil
+	defer lis.Close()
+	return http.Serve(lis, handler)
 }
 
 func listenHTTP(srv *tsnet.Server, handler http.Handler, addr string) error {
-	httpListener, err := srv.Listen("tcp", addr)
+	lis, err := srv.Listen("tcp", addr)
 	if err != nil {
 		return err
 	}
-	defer httpListener.Close()
-	if err := http.Serve(httpListener, handler); err != nil {
-		return err
-	}
-	return nil
-
-}
-
-// HSTS wraps the provided handler and sets Strict-Transport-Security header on
-// responses. It inspects the Host header to ensure we do not specify HSTS
-// response on non fully qualified domain name origins.
-func HSTS(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		host, found := r.Header["Host"]
-		if found {
-			host := host[0]
-			fqdn, err := dnsname.ToFQDN(host)
-			if err == nil {
-				segCount := fqdn.NumLabels()
-				if segCount > 1 {
-					w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-				}
-			}
-		}
-		h.ServeHTTP(w, r)
-	})
+	defer lis.Close()
+	return http.Serve(lis, handler)
 }
 
 // redirectHandler returns the http.Handler for serving all plaintext HTTP
 // requests. It redirects all requests to the HTTPs version of the same URL.
 func redirectHandler(hostname string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, (&url.URL{Scheme: "https", Host: hostname, Path: r.URL.Path}).String(), http.StatusFound)
+		http.Redirect(w, r,
+			(&url.URL{Scheme: "https", Host: hostname, Path: r.URL.Path}).String(),
+			http.StatusPermanentRedirect)
 	})
 }
 
